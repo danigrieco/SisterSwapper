c2cac0751e472f920d5b3afa1c9a611172912e3f
griec <tgh8wp@virginia.edu>
2023-06-22 15:57:42 -0400

    GOT APP WORKING!!

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
 module SisterSwapper.main {
     requires java.sql;
-    //requires org.junit.jupiter.api;
+    requires org.junit.jupiter.api;
     requires javafx.graphics;
     requires javafx.fxml;
     requires javafx.controls;
     opens edu.virginia.cs.gui to javafx.fxml;
-    //exports edu.virginia.cs.gui;
+    exports edu.virginia.cs.gui;
 }
